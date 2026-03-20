d4c4b5b2dd4b3e086b61361910df8d20b6115e3d
Anmol <anmolkumar2633@gmail.com>
2026-03-21 04:27:25 +0530

    Added landing page UI with blue bground

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './features/home/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  }
+];
